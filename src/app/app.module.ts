764cfbca951cb7be5dcd87d9f5683fad12a9e718
Sonia Ramiro <soniaramiro@MBP-de-Sonia.home>
2017-08-08 18:07:23 +0200

    Componente empleado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FrutaComponent } from './fruta/fruta.component';
+import { empleadoComponent } from './empleado/empleado.component';
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent, FrutaComponent],
+  declarations: [AppComponent, FrutaComponent, empleadoComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component ({
+    selector: 'empleado',
+    templateUrl: './empleado.component.html'
+})
+
+export class empleadoComponent{
+    public titulo =  'Titulo del componente empleado';
+}
