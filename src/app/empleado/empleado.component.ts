c502997b2d8418a0150f279c68cfb0c3138b5e83
Sonia Ramiro <soniaramiro@MBP-de-Sonia.home>
2017-09-24 19:56:59 +0200

    modelos

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Empleado} from './empleado';
 
 @Component ({
     selector: 'empleado',
@@ -7,4 +8,17 @@ import {Component} from '@angular/core';
 
 export class empleadoComponent{
     public titulo =  'Titulo del componente empleado';
+
+    public empleado: Empleado;
+    public trabajadores: Array<Empleado>;
+
+    ngOnInit(){
+        this.empleado = new Empleado('Sonia', 28);
+        console.log(this.empleado);
+        this.trabajadores = [
+            new Empleado('Ana', 26),
+            new Empleado('Edu', 29),
+            new Empleado('Sofia', 27)
+        ];
+    }
 }
