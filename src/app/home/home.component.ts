62a167cd7d051a31c7bc30124fd5af43397e2057
Sonia Ramiro <soniaramiro0@gmail.com>
2017-11-05 18:36:33 +0100

    pipes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Ropaservice } from '../service/ropa.service';
+
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    providers: [Ropaservice]
 })
 
 export class HomeComponent {
     public titulo = 'home';
+    public fecha;
+
+    constructor(
+        private _ropaService: Ropaservice,
+
+    ) {
+        this.fecha = new Date('2017-04-02');
+     }
+
+    ngOnInit(){
+        console.log(this._ropaService);
+    }
+
 };
 
